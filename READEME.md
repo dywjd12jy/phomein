e26d6f37993344660b6871cf2039e7b6cf377b19
dywjd12jy <89719682+dywjd12jy@users.noreply.github.com>
2021-12-22 23:41:18 +0900

    first commit

---
+++ READEME.md
@@ -0,0 +1,3 @@
+# phomein copy
+
+- phomein 사이트 클론코딩 입니다.
---
+++ js/main.js
@@ -0,0 +1,22 @@
+// $(document).ready(function(){
+  
+//   // fullpage customization
+//   // $('#fullpage').fullpage({
+//   //   sectionSelector: '.vertical-scrolling',
+//   //   slideSelector: '.horizontal-scrolling',
+//   //   navigation: true,
+//   //   slidesNavigation: true,
+//   //   css3: true,
+//   //   controlArrows: false    
+//   // });
+  
+// });
+$(function(){
+  
+  $('#fullpage').fullpage({
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    navigation: true,
+  });
+});
