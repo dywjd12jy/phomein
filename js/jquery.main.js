105da48674b8867e04765168cfb9e180c9f54e61
dywjd12jy <89719682+dywjd12jy@users.noreply.github.com>
2021-12-29 04:09:48 +0900

    nav , scroll error 해결

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,17 +1,33 @@
-$(document).ready(function () 
-{
+
+
+$(function(){
+
+    var Header = $('.header')
     var NavBar = $('.navbar')
     var Nav = $('.nav')
     var Nav_Menu = $('.nav_menu')
-    Nav.hover(function(){
-        NavBar.css('top','100%');
-        NavBar.css('opacity','1');
-        Nav_Menu.css('top','100%');
-        Nav_Menu.css('opacity','1');
-    }, function() {
-        NavBar.css('top','-100%');
-        NavBar.css('opacity','0');
-        Nav_Menu.css('top','-300%');
-        Nav_Menu.css('opacity','0');
+
+    // $Header.on("mouseover",function(){
+    //     $(this).removeClass("")
+    // });
+
+    Nav.on("mouseover",function(){
+        Header.css('background-color','rgba(0,0,0,0.9)'),
+        NavBar.css('height','260%'),
+        NavBar.css('padding','60rem 0 0 0'),
+        NavBar.css('opacity','1'),
+        Nav_Menu.css('padding','20rem 0'),
+        Nav_Menu.css('opacity','1'),
+        Nav_Menu.css('top','100%')
+
+    }); 
+    Nav.on("mouseout",function(){
+        Header.css('background-color','rgba(0,0,0,0)'),
+        NavBar.css('height','0'),
+        NavBar.css('padding','0'),
+        NavBar.css('opacity','0'),
+        Nav_Menu.css('padding','0'),
+        Nav_Menu.css('opacity','0'),
+        Nav_Menu.css('top','-200%')
     });
 });
--- js/main.js
+++ js/main.js
@@ -12,13 +12,16 @@
   
 // });
   $(function(){
-
+    
     $('#fullpage').fullpage({
       //options here
       autoScrolling:true,
       scrollHorizontally: true,
       navigation: true, 
       navigationTooltips: ['홈','브랜드','메뉴','서비스','사회공헌','SNS','커뮤니티','footer'],
+      anchors: ['section1','section2','section3','section4','section5','section6','section7'],
       showActiveTooltip: true,
+      bigSectionsDestination: top
     });
   });
+  
