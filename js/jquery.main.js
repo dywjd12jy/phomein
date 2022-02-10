cc547d7a6cc04c5004e9a872ce87e3331dacf40f
정요 <jeong-yo@jeong-youi-MacBookAir.local>
2022-02-10 11:27:14 +0900

    포메인 진행중

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,16 +1,11 @@
 
 
-$(function(){
-
+$(window).resize(function() { if($(window).width() > 1019) {
     var Header = $('.header')
     var NavBar = $('.navbar')
     var Nav = $('.nav')
     var Nav_Menu = $('.nav_menu')
-
-    // $Header.on("mouseover",function(){
-    //     $(this).removeClass("")
-    // });
-
+    
     Nav.on("mouseover",function(){
         Header.css('background-color','rgba(0,0,0,0.9)'),
         NavBar.css('height','260%'),
@@ -29,5 +24,33 @@ $(function(){
         Nav_Menu.css('padding','0'),
         Nav_Menu.css('opacity','0'),
         Nav_Menu.css('top','-200%')
-    });
-});
+    }); 
+} });
+$(window).resize(function() { if($(window).width() <= 1019) {
+    var Header = $('.header')
+    var NavBar = $('.navbar')
+    var Ham = $('.ham')
+    var Nav_Menu = $('.nav_menu')
+
+    Ham.on.toggle
+
+    // Nav.on("mouseover",function(){
+        // Header.css('background-color','rgba(0,0,0,0.9)'),
+        // NavBar.css('height','260%'),
+        // NavBar.css('padding','60rem 0 0 0'),
+        // NavBar.css('opacity','1'),
+        // Nav_Menu.css('padding','20rem 0'),
+        // Nav_Menu.css('opacity','1'),
+        // Nav_Menu.css('top','100%')
+
+    // }); 
+    // Nav.on("mouseout",function(){
+        // Header.css('background-color','rgba(0,0,0,0)'),
+        // NavBar.css('height','0'),
+        // NavBar.css('padding','0'),
+        // NavBar.css('opacity','0'),
+        // Nav_Menu.css('padding','0'),
+        // Nav_Menu.css('opacity','0'),
+        // Nav_Menu.css('top','-200%')
+    // }); 
+} });
